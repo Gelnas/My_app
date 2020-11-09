7d3d8d35f0e01bd5ae462487924436b3a4b792d3
Gelnas <47691659+Gelnas@users.noreply.github.com>
2020-11-09 19:42:32 +0400

    new

--- src/main/java/com/simbirsoft/my_app/Controller.java
+++ src/main/java/com/simbirsoft/my_app/Controller.java
@@ -4,6 +4,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,10 @@ public class Controller {
     @GetMapping("/hey")
     public ResponseEntity<String> sayHey(){
         return new ResponseEntity<String>("Hi", HttpStatus.OK);
+    }
+
+    @PostMapping("/hey")
+    public ResponseEntity<String> sayHey1(){
+        return new ResponseEntity<String>("Hi1", HttpStatus.OK);
     }
 }
